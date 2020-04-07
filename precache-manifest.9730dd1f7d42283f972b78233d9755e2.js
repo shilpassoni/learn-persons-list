f658e075fc862de1c4ecb210383160421ae137e6
Shilpa Soni <shilpassoni@gmail.com>
2020-04-07 18:11:09 +0530

    Updates

---
+++ precache-manifest.9730dd1f7d42283f972b78233d9755e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d0e4548032c4f4adc9b0a20668c272c",
+    "url": "/learn-persons-list/index.html"
+  },
+  {
+    "revision": "d2402874f1649f80e2d3",
+    "url": "/learn-persons-list/static/css/main.ae0aa742.chunk.css"
+  },
+  {
+    "revision": "6d5154f9f08820c9aeda",
+    "url": "/learn-persons-list/static/js/2.6767719c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learn-persons-list/static/js/2.6767719c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2402874f1649f80e2d3",
+    "url": "/learn-persons-list/static/js/main.3f7b63e4.chunk.js"
+  },
+  {
+    "revision": "97e58fb3ebcdbd98e0b7",
+    "url": "/learn-persons-list/static/js/runtime-main.e29586b0.js"
+  }
+]);
